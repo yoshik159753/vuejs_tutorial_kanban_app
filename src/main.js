d93d8e9e12e11acfbfce56a25d190dad072e3340
yoshik <kai.outsider.777+git@gmail.com>
2019-02-22 04:36:27 +0000

    8.8.5 状態管理ライブラリの導入
    ```
    npm install --save vuex es6-promise
    ```

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import 'es6-promise/auto' // プロミスをポリフィルする
 import App from './App'
 import router from './router'
+import store from './store' // Vuex のストアインスタンスをインポート
 
 Vue.config.productionTip = false
 
@@ -8,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store, // インポートしたストアインスタンスを `store` オプションとして指定
   render: h => h(App)
 })
