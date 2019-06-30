b1cc39e67066f799b91fae6c537b2597461963d6
YoshiK <kai.outsider.777+git@gmail.com>
2019-07-01 04:02:36 +0900

    10.5.1 開発サーバーによる開発

--- build/dev-server.js
+++ build/dev-server.js
@@ -8,5 +8,27 @@ module.exports = app => {
   // HTTP リクエストの body の内容を JSON として解析するようミドルウェアをインストール
   app.use(bodyParser.json())
 
-  // TODO : ここ以降に API の実装内容を追加していく
+  // ユーザー情報
+  const users = {
+    'foo@domain.com': {
+      password: '12345678',
+      userId: 1,
+      token: '1234567890abcdef'
+    }
+  }
+
+  // ログイン API のエンドポイント '/auth/login'
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if (user) {
+      if (user.password !== password) {
+        res.status(401).json({ message: 'ログインに失敗しました。' })
+      } else {
+        res.json({ userId: user.userid, token: user.token })
+      }
+    } else {
+      res.status(404).json({ message: 'ユーザーが登録されていません。' })
+    }
+  })
 }
